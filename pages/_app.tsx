e9d3ef188c8c647ea104952983366679bfabd24b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-09-18 16:39:10 +0100

    add aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />

57e53000ec1e00092b15a01ec1b4b92dae686fd3
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-25 09:33:52 +0100

    import all landingpage component

---
+++ components/landingpage/index.ts
@@ -0,0 +1,9 @@
+
+export * from './Header';
+export * from './Hero';
+export * from './Features';
+export * from './Testimonials';
+export * from './Faq';
+export * from './CTA';
+export * from './Footer';
+export * from './Pricing';

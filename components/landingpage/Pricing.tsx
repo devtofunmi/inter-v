8173f131b016b5b0078a3389e624547e48b7bd87
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-10-25 11:40:29 +0100

    changed all green text and hover color to blue

--- components/landingpage/Pricing.tsx
+++ components/landingpage/Pricing.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import Link from "next/link";
 
@@ -11,25 +10,25 @@ export const Pricing = () => {
       <div className="grid sm:grid-cols-2 gap-8 max-w-4xl mx-auto">
         <div 
           onClick={() => setSelectedPlan(0)}
-          className={`p-8 bg-white rounded-xl  flex flex-col items-center cursor-pointer transition-all duration-300 ${selectedPlan === 0 ? 'border-2 border-emerald-600 shadow-xl' : 'border border-gray-200'}`}
+          className={`p-8 bg-white rounded-xl  flex flex-col items-center cursor-pointer transition-all duration-300 ${selectedPlan === 0 ? 'border-2 border-blue-400 shadow-xl' : 'border border-gray-200'}`}
         >
           <h3 className="text-2xl font-bold mb-2 text-gray-900">Free Plan</h3>
           <p className="text-gray-600 mb-4 text-lg font-normal">Ideal for introductory practice sessions.</p>
           <p className="text-5xl font-extrabold mb-6 text-gray-900">$0</p>
           <Link href="/auth">
-            <button className="px-6 py-3 bg-emerald-600 cursor-pointer rounded-full hover:bg-emerald-700 text-white font-semibold shadow-md transition-colors text-lg">
+            <button className="px-6 py-3 bg-blue-400 cursor-pointer rounded-full hover:bg-blue-500 text-white font-semibold shadow-md transition-colors text-lg">
               Get Started
             </button>
           </Link>
         </div>
         <div 
           onClick={() => setSelectedPlan(1)}
-          className={`p-8 bg-white rounded-xl flex flex-col items-center cursor-pointer transition-all duration-300 ${selectedPlan === 1 ? 'border-2 border-emerald-600 shadow-xl' : 'border border-gray-200'}`}
+          className={`p-8 bg-white rounded-xl flex flex-col items-center cursor-pointer transition-all duration-300 ${selectedPlan === 1 ? 'border-2 border-blue-400 shadow-xl' : 'border border-gray-200'}`}
         >
           <h3 className="text-2xl font-bold mb-2 text-gray-900">Pro Plan</h3>
           <p className="text-gray-600 mb-4 text-lg font-normal">Unlock full platform capabilities and unlimited interviews.</p>
           <p className="text-5xl font-extrabold mb-6 text-gray-900">$8/mo</p>
-          <button className="px-6 py-3 cursor-not-allowed opacity-60 bg-emerald-600 rounded-full text-white font-semibold shadow-md text-lg">
+          <button className="px-6 py-3 cursor-not-allowed opacity-60 bg-blue-400 rounded-full text-white font-semibold shadow-md text-lg">
             Upgrade Now
           </button>
         </div>

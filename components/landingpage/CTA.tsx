a199bab067c833637e5b8bb7b7dc8c44a6773a9a
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-11-05 15:58:54 +0100

    change p text

--- components/landingpage/CTA.tsx
+++ components/landingpage/CTA.tsx
@@ -6,8 +6,8 @@ export const CTA = () => {
     <section id="cta" className="px-6 py-20 bg-gray-100 text-center" data-aos="fade-up">
       <div className="max-w-4xl mx-auto">
         <h2 className="text-5xl font-extrabold mb-4 text-gray-900">Ready to Master Your Next Interview?</h2>
-        <p className="text-xl text-gray-600 mb-8 font-normal">
-          Join thousands of professionals who have aced their interviews with Prepkitty.
+        <p className="text-xl text-gray-600 mb-4 font-normal">
+          Join thousands of professionals who have aced their interviews with Prepkitty. Perfect for job seekers and graduates 
         </p>
         <Link href="/signup">
           <button className="px-8 py-4 rounded-full bg-blue-400 hover:bg-blue-500 text-white text-xl font-bold shadow-xl transition-all duration-300 transform hover:scale-105">

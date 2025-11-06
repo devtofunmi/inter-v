27c1172588982b64ce73a4decb043d7a0d488fa5
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-11-06 15:59:13 +0100

    added and use head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 
+import Head from "next/head";
 import {
   Header,
   Hero,
@@ -13,6 +14,9 @@ import {
 export default function Home() {
   return (
     <div className="font-geist bg-white text-gray-900 text-[17px] font-normal">
+      <Head>
+        <title>Prepkitty - AI Interview Practice</title>
+      </Head>
       <Header />
       <Hero />
       <Features />
